adbe4d478f21f4d787941db69c113422bcce6d11
Adriano-Paz <adriano.agp15@gmail.com>
2024-10-30 21:22:07 -0300

    [Trabalho 06] Esta trabalho é referente a atividade 06 de acessibilidade  referente a melhorias no meta,head,heading,figure e figcaption

---
+++ trabalhos/trabalho_06/art_daniel_esteves.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <?php include 'includes/head.php';?>
+    <link rel="stylesheet" href="css/paginaartistas.css">
+    <title>Daniel Esteves</title>
+</head>
+<body>
+    <header>
+        <?php include 'includes/header.php';?>
+    </header>
+
+    </div>
+    <div class="container corpo espaco p-5">
+
+
+    <div class="row text-center p-5">
+        <div class="col-lg-6 col-md-12 col-sm-12 foto">
+        <section>
+            <h1 hidden> Foto Daniel Esteves</h1>
+            <img src="img/esteves/perfil.png" alt="foto do daniel" class="img_perfil centralizador">
+        </section>
+        </div>
+
+        <div  class="col-lg-6 col-md-12 col-sm-12 dados ">
+        <section>
+            <h1 class="Nome">Daniel Esteves</h1>
+            <p class="info_geral"><img src="https://img.icons8.com/?size=50&id=32309&format=png&color=000000" alt="para instagram de Daniel Esteves" class="logo_insta">@zapata.edicoes</p>
+        </section>
+        </div>
+    </div>
+
+        <section class="descricao">
+            <h1 hidden>Descrição</h1>
+            <p class="fs-5">Daniel Esteves(<a href="https://www.instagram.com/zapata.edicoes/">@zapata.edicoes</a>) Daniel Esteves ou “zapata edições”,
+            Formado no curso técnico de Desenho de Comunicação e graduado em História pela USP, Daniel Esteves fundou a editora independente Zapata Edições e o curso HQ em Foco, dedicado ao ensino de temas relacionados às histórias em quadrinhos, além de ter sido membro dos coletivos Petisco e Quarto Mundo. Também já escreveu roteiros para coletâneas como Front, Quadreca, Garagem Hermética, Café Espacial, entre outras</p>
+            <p class="fs-5"> Em 2013, Esteves lançou o primeiro volume da série São Paulo dos Mortos, que mostra um futuro próximo pós-apocaliptico no qual o mundo foi tomado por zumbis. A trama, ambientada em São Paulo, tem a participação de diversos desenhistas, como Al Stefano, Alex Rodrigues, Jozz, Ibraim Roberson, Lucas Perdomo, Laudo Ferreira, Omar Viñole, Samuel Bono, entre outros.</p>
+            <p class="fs-6 text-muted"><p class="fs-6 text-muted"><small>Biografia Daniel Esteves, .[S. l.], Disponível
+            em: https://www.instagram.com/zapata.edicoes/ Acesso:  17 jun. 2024.</small></p>
+        </section>
+
+        <div class="row text-center">
+            <h2 class="obras">Obras:</h2>
+        </div>
+        <div class="row text-center">
+            <div class="col-lg-4 col-md-6 col-sm-12 p-2-livros">
+            <section>
+                <h1 hidden>São Paulo dos Mortos Vivos</h1>
+                <figure>
+                <a href="quadrinhoBase_spmortos.php">
+                    <img src="img/esteves/mortos.jpg" alt="Livro 'São paulo dos mortos' de Daniel esteves" class="obras_artista">
+                </a>
+                <figcaption>São Paulo dos Mortos Vivos</figcaption>
+                <figure>
+            </section>
+            </div>
+
+            <div class="col-lg-4 col-md-6 col-sm-12 p-2-livros">
+            <section>
+                <h1 hidden>Por mais um dia com Zapata</h1>
+                <figure>
+                <a href="quadrinhoBase_zapata.php">
+                    <img src="img/esteves/zapata.jpg" alt="Livro 'Por mais um dia com Zapata' de Daniel Esteves" class="obras_artista">
+                </a>
+                <figcaption>Por Mais um dia com Zapata</figcaption>
+                </figure>
+            </section>
+            </div>
+
+            <div class="col-lg-4 col-md-12 col-sm-12 p-2-livros">
+            <section>
+                <h1 hidden>As Aventuras do Capitão Nemo</h1>
+                <figure>   
+                <a href="quadrinhoBase_nemo.php">
+                    <img src="img/esteves/nemo.jpg" alt="Livro 'As aventuras do capitão Nemo ' de Daniel Esteves" class="obras_artista">
+                </a>
+                <figcaption>As Aventuras do Capitão Nemo</figcaption>
+                </figure>
+            </section>
+            </div>
+        </div>
+
+    </div>
+
+    <footer>
+    <?php include 'includes/footer.php';?>
+    </footer>
+</body>
+</html>
