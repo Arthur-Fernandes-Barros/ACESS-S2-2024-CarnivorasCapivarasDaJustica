f192c20eb8837c565a6655a85ad75ab4569ce375
Adriano-Paz <adriano.agp15@gmail.com>
2024-11-28 20:05:02 -0300

    Entrega pastas css, img, includes

---
+++ trabalhos/trabalho_09/includes/header.php
@@ -0,0 +1,51 @@
+<div class="row bglogo">
+            <div class="col col-5">
+                <img src="img/logoFatecon.png" alt="logo da fatecon"  class="logofatecon">
+            </div>
+        </div>
+<nav role="navigation" class="navbar navbar-expand-lg">
+  <!--menu de navegação-->
+  <div class="container-fluid">
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavDropdown">
+        
+    <ul class="navbar-nav">
+            <li class="nav-item borderli">
+                <a class="nav-link " aria-current="page" href="index.php" aria-label="para homepage">Inicio</a>
+            </li>
+            <li class="nav-item borderli">
+          <a class="nav-link " href="quadrinhos.php" aria-label=" para página quadrinhos em destaques">Quadrinhos em Destaque</a>
+        </li>
+        <li class="nav-item borderli">
+          <a class="nav-link" href="artistas.php" aria-label="para página Asrtistas">Artistas</a>
+        </li>
+        <li class="nav-item dropdown borderli">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false" aria-label="link galeria com dropdown">
+            Galeria
+          </a>
+        <ul class="dropdown-menu">  
+            <li><a class="dropdown-item" href="galeriacosplay.php" aria-label="para página galeria cosplay">Cosplays</a></li>
+            <li><a class="dropdown-item" href="galerialocal.php" aria-label=" para página quadrinhos">Quadrinhos</a></li>
+          </ul>
+        </li>
+        <li class="nav-item borderli">
+          <a class="nav-link" href="historia.php" aria-label="para página história">História</a>
+        </li>
+        <li class="nav-item borderli">
+          <a class="nav-link" href="evento.php" aria-label="para página eventos">Eventos</a>
+        </li>
+        <li class="nav-item borderli">
+          <a class="nav-link" href="contato.php" aria-label="para página contatos">Contatos</a>
+        </li>
+        <li class="nav-item borderli">
+          <a class="nav-link" href="about.php" aria-label="para página quem somos">Quem Somos</a>
+        </li>
+            
+            
+     </ul>
+    </div>
+  </div>
+</nav>
+
